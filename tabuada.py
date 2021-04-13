2e64b69c9c24b9906b522047fb9e0694f7937dc9
Thiago Tuissi Paião <81596921+ThiagoTuissiPaiao@users.noreply.github.com>
2021-04-12 23:21:09 -0300

    Add files via upload

---
+++ tabuada.py
@@ -0,0 +1,28 @@
+num = int(input('Digite um número para ver a sua tabuada: '))
+print('{} x {:2} = {}'.format(num, 1, num*1))
+print('{} x {:2} = {}'.format(num, 2, num*2))
+print('{} x {:2} = {}'.format(num, 3, num*3))
+print('{} x {:2} = {}'.format(num, 4, num*4))
+print('{} x {:2} = {}'.format(num, 5, num*5))
+print('{} x {:2} = {}'.format(num, 6, num*6))
+print('{} x {:2} = {}'.format(num, 7, num*7))
+print('{} x {:2} = {}'.format(num, 8, num*8))
+print('{} x {:2} = {}'.format(num, 9, num*9))
+print('{} x {:2} = {}'.format(num, 10, num*10))
+
+"""
+# Segunda opção
+numero = int(input("Digite o numero da tabuada: "))
+
+print(f"`{numero} x 1 = {numero * 1}")
+print(f"`{numero} x 1 = {numero * 2}")
+print(f"`{numero} x 1 = {numero * 3}")
+print(f"`{numero} x 1 = {numero * 4}")
+print(f"`{numero} x 1 = {numero * 5}")
+print(f"`{numero} x 1 = {numero * 6}")
+print(f"`{numero} x 1 = {numero * 7}")
+print(f"`{numero} x 1 = {numero * 8}")
+print(f"`{numero} x 1 = {numero * 9}")
+print(f"`{numero} x 1 = {numero * 10}")
+
+"""
